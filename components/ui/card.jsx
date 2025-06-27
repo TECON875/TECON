852eeddd5ac9b5c00dbc3cee23726bb35aad5613
TECON875 <asalles598@gmail.com>
2025-06-27 11:31:45 -0300

    Create card.jsx

---
+++ components/ui/card.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Card({ children }) {
+  return <div className="rounded-2xl shadow-md bg-white">{children}</div>;
+}
+
+export default Card;
+
+criar componente Card
